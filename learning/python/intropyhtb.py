c852dab9e0334d27e0173e93facff55c979fabb7
JuliCC07 <lca6822@gmail.com>
2026-02-17 10:33:25 +0100

    Intro Python HackTheBox

---
+++ learning/python/intropyhtb.py
@@ -0,0 +1,33 @@
+# Welcome
+## print("Hello Academy!")
+# Hello Again, Academy
+## greeting = "Hello again, Academy"
+## print(greeting)
+## a = "HTB"
+## b = "Academy"
+## print(a, b)
+## Use #!/usr/bin/env python3 on Linux to execute the code without typing "python" in the terminal.
+# A Few Simple Variables
+## advice = "Don't panic"
+## ultimate_answer = 42
+## potential_question =  6 * 7
+## confident = True
+## something_false = False
+## problems = None
+# Latest expression can be the variable "_" in IDLE i.e:
+## 38 + 4
+## 50 - _
+# Conditional statements and Loops
+## happy = True
+## if happy: # Shorthand of happy == True
+##     print("Happy and we know it!")
+## else:
+##     print("Not happy...")
+## 
+## happy = 2
+## if happy == 1:
+##     print("Happy and we know it !")
+## elif happy == 2:
+##     print("Excited about it!")
+## else:
+##     print("Not happy...")
